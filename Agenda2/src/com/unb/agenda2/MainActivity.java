5b662c24e41a82e8b1d2bc2c1c3ccadce43010af
Guilherme <Guilherme@192.168.1.3>
2012-10-22 18:25:21 -0200

    Segundo Commit

--- Agenda2/src/com/unb/agenda2/MainActivity.java
+++ Agenda2/src/com/unb/agenda2/MainActivity.java
@@ -1,20 +1,28 @@
 package com.unb.agenda2;
 
+import com.unb.agenda2.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
+      
+    
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+   
+    
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    
+   */
+
     }
 }
